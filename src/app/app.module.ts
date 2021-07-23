1e5cfdcedc75efc174306f2e73ce6e6022a9bf68
suchat.kl <suchat.kl@kkumail.com>
2021-07-23 12:23:00 +0700

    update menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {InputTextModule} from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+
+import {MenubarModule} from 'primeng/menubar';
+import {MenuItem} from 'primeng/api';
+import {PasswordModule} from 'primeng/password';
+import {ButtonModule} from 'primeng/button';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
+
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InputTextModule,
+    FormsModule,
+    MenubarModule,
+    PasswordModule,
+    ButtonModule,
+    BrowserAnimationsModule,
+    AccordionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
