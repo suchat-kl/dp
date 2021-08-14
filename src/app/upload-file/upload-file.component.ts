8c120d5aaee487a1f5a7e0106787ab42f7494520
suchat-kl <suchat.kl@kkumail.com>
2021-08-15 01:20:53 +0700

    updateDateStr

--- src/app/upload-file/upload-file.component.ts
+++ src/app/upload-file/upload-file.component.ts
@@ -18,6 +18,7 @@ import { YtServiceService } from '../yt-service.service';
 export class UploadFileComponent implements OnInit {
   disableInsert: boolean = true;
   disableDelete: boolean = true;
+  disableUpdate: boolean = true;
   // sizeMax = false;
   // isTxt = false;
   sizeMaxFile = 5;
@@ -44,12 +45,13 @@ export class UploadFileComponent implements OnInit {
   constructor(private routeA: ActivatedRoute, private ytSv: YtServiceService,
     private route: Router, private http: HttpClient
   ) { }
-  base64: any;
+  base64: any; //dateNum: String = "";
   fileName: string = ""; yearTax: string = ""; fileSize: string = "";
   //fileGloblal: File | undefined;
   // maxSize=5*1024*1024;
   onChange = (event: Event) => {
     //let FS = require('fs');
+    let dStr = "";
     const target = event.target as HTMLInputElement;
     const file: File = (target.files as FileList)[0];
     this.display = false;
@@ -104,13 +106,19 @@ export class UploadFileComponent implements OnInit {
         if (data[10] == null || typeof data[10] === 'undefined') { //yearTax
           continue;
         }
+        if (sessionStorage.getItem("dStr") == null) {
+          // this.dateNum = data[19];
+          sessionStorage.setItem("dStr", data[19]);
+          dStr = this.updateDateStr();
+          //console.log(data[19]);
+        }
         // console.log(data); .push
         this.jsonOutputArr.push({
           "xx": data[5], "a": data[0], "b": data[1], "c": data[2],
           "d": data[3], "e": data[4], "f": data[6], "g": data[7], "h": data[8],
           "i": data[9], "j": data[10], "k": data[11], "l": data[12], "m": data[13],
           "n": data[14], "o": data[15], "p": data[16], "q": data[17], "r": data[18],
-          "s": data[19]
+          "s": dStr
         });
 
         // break;
@@ -120,79 +128,88 @@ export class UploadFileComponent implements OnInit {
       // console.log((this.jsonOutput));
       this.disableDelete = false;
     };
-
+    //console.log(this.dateNum);
+    //this.updateDateStr();
 
   };
+  updateDateStr() {
+    let monthStr =
+      [
+        "มกราคม",
+        "กุมภาพันธ์",
+        "มีนาคม",
+        "เมษายน",
+        "พฤษภาคม",
+        "มิถุนายน",
+        "กรกฏาคม",
+        "สิงหาคม",
+        "กันยายน",
+        "ตุลาคม",
+        "พศจิกายน",
+        "ธันวาคม"
+      ];
+
+    // let i:number=0;
+    // i=Number(this.dateNum.substring(2,4));
+    let s = sessionStorage.getItem("dStr") + "";
+    sessionStorage.removeItem("dStr");
+    let key: any = Number(s.substring(2, 4));
+    // if (key===6){
+    //   alert(key);
+    // }
+    // console.log(s);
+    // console.log(key);
+    //console.log(this.jsonOutputArr[0]["s"]);
+    let x = monthStr[key - 1]
+
+    // console.log(x);
+
+
+
+    //monthStr[6][this.dateNum.substring(2,4)]
+    //console.log(key);
+    let dStr =  s.substring(0, 2) + " /  " + x + "  / " + s.substring(4) ;
+
+    return dStr;
+
+
+  }
   async onclick(value: any) {
     // let data = JSON.stringify(value);
     // console.log(data);
     // "fileName": this.yearTax,
     // "base64": this.base64,
     // "yeartax": this.yearTax,
-if (!this.disableDelete) {
-    let url = "http://dbdoh.doh.go.th:9999/delYT/" + this.yearTax;//+"?file="+this.fileGloblal;
+    if (!this.disableDelete) {
+      let url = "http://dbdoh.doh.go.th:9999/delYT/" + this.yearTax;//+"?file="+this.fileGloblal;
 
 
-    let header = {
-      headers: new HttpHeaders()
-        .set('Authorization', "Bearer " + sessionStorage.getItem("token"))
-      // .set('Content-Type', 'application/json')
-      // .set('Accept', 'application/json')   
-      // .set('Access-Control-Allow-Origin', '*')
-      // .set('Access-Control-Allow-Credentials', 'true')     
-      // .set("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS")
-      // .set("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
+      let header = {
+        headers: new HttpHeaders()
+          .set('Authorization', "Bearer " + sessionStorage.getItem("token"))
+        // .set('Content-Type', 'application/json')
+        // .set('Accept', 'application/json')   
+        // .set('Access-Control-Allow-Origin', '*')
+        // .set('Access-Control-Allow-Credentials', 'true')     
+        // .set("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS")
+        // .set("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
 
 
-    }
-    // console.log(body);
-
-
-
-    try {
-      await this.http.delete(url, header).toPromise().
-        then(response => {
-          // let j = JSON.stringify(response);
-          // let obj2: LoginApi = JSON.parse(j);
-          // console.log(response);
-          // console.log("success");
-          this.display = true; this.msg_err = "ลบข้อมูลเสร็จแล้ว....";
-          this.disableDelete=true;
-          this.disableInsert=false;
-        });
-
-    }
-    catch (err) {
-
-      // this.display = true;
-      console.log("error");
-      console.log(err);
-
-    }
-    
-    return;
-  } //delete
+      }
+      // console.log(body);
 
 
 
-if (!this.disableInsert){
-    // console.log(body);
-  let  url = "http://dbdoh.doh.go.th:9999/insertTxt";
-
-    for (let i = 0; i < this.jsonOutputArr.length; i++) {
-      this.jsonOutput = this.jsonOutputArr[i];
-      let body = this.jsonOutput;
-      try {//,header
-        await this.http.post(url, body).toPromise().
+      try {
+        await this.http.delete(url, header).toPromise().
           then(response => {
             // let j = JSON.stringify(response);
             // let obj2: LoginApi = JSON.parse(j);
             // console.log(response);
             // console.log("success");
-            //this.route.navigate(['']);
-            this.display = true; this.msg_err = "เพิ่มข้อมูลจากไฟล์ข้อความเสร็จแล้ว....";
-            this.disableInsert=true;
-            this.disableDelete=true;
+            this.display = true; this.msg_err = "ลบข้อมูลเสร็จแล้ว....";
+            this.disableDelete = true;
+            this.disableInsert = false;
           });
 
       }
@@ -204,13 +221,55 @@ if (!this.disableInsert){
 
       }
 
+      return;
+    } //delete
 
-    }
 
-  }//insert
+
+    if (!this.disableInsert) {
+      // console.log(body);
+      let url = "http://dbdoh.doh.go.th:9999/insertTxt";
+
+      for (let i = 0; i < this.jsonOutputArr.length; i++) {
+        this.jsonOutput = this.jsonOutputArr[i];
+        let body = this.jsonOutput;
+        try {//,header
+          await this.http.post(url, body).toPromise().
+            then(response => {
+              // let j = JSON.stringify(response);
+              // let obj2: LoginApi = JSON.parse(j);
+              // console.log(response);
+              // console.log("success");
+              //this.route.navigate(['']);
+              // this.disableUpdate = false;
+              this.display = true; this.msg_err = "เพิ่มข้อมูลจากไฟล์ข้อความเสร็จแล้ว....";
+              this.disableInsert = true;
+              this.disableDelete = true;
+              return;
+            });
+
+        }
+        catch (err) {
+
+          // this.display = true;
+          console.log("error");
+          console.log(err);
+
+        }
+
+
+      }
+
+    }//insert
     // document.getElementById("file-id").files[0].name; 
     // let obj: UsrpwdForm = JSON.parse(this.data);
     //{"filetxt":"C:\\fakepath\\หนังสือรับรองภาษี 2563_New.txt"}
+    if (!this.disableUpdate) {
+      this.updateDateStr();
+      this.display = true; this.msg_err = "ปรับปรุงข้อมูลจากไฟล์ข้อความเสร็จแล้ว....";
+      this.disableUpdate = true;
+    }
+
   }
 
 
